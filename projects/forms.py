35244aebff2f3ac36e22bab720f391d44f53d154
Shriram Mogal <shriram_mogal@persistent.com>
2021-12-28 16:05:12 +0530

    Made UI Modification

--- projects/forms.py
+++ projects/forms.py
@@ -1,8 +1,15 @@
+
 from django.db import models
 from django.forms import ModelForm, fields
+from django.forms.widgets import CheckboxSelectMultiple
 from .models import Project
+from django import forms
 
 class ProjectForm(ModelForm):
     class Meta:
         model = Project
-        fields = ['title', 'featured_image','decription', 'demo_link', 'sorce_link', 'tag']
+        fields = ['title', 'featured_image','decription','source_link', 'demo_link', 'tag']
+        
+        widgets = {
+            'tag' : forms.CheckboxSelectMultiple(),
+        }
--- projects/models.py
+++ projects/models.py
@@ -16,7 +16,7 @@ class Project(models.Model):
     title = models.CharField(max_length=200)
     decription = models.TextField(null=True, blank=True)
     demo_link = models.CharField(max_length=2000, null=True, blank=True)
-    sorce_link = models.CharField(max_length=2000, null=True, blank=True)
+    source_link =models.CharField(max_length=2000, null=True, blank=True)
     featured_image = models.ImageField(null= True, blank=True, default='default.jpg')
     tag = models.ManyToManyField(Tag, blank= True)
     vote_total = models.IntegerField(default=0, null= True, blank=True)
